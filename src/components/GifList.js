8ca3509c56d44894c6335f9f8cf7eaa8ec1a1809
Zach Lefler <leaving.valhalla@gmail.com>
2022-04-11 15:16:36 -0700

    commit tree d01b7305812e86b9b93a9115a143a6b5741b35d7

--- src/components/GifList.js
+++ src/components/GifList.js
@@ -1,8 +1,7 @@
 import React from 'react';
 
 function GifList({ gifs }) {
-  console.log(gifs[0]);
-  return gifs.forEach((gif) => <img key={gif} src={gif} alt={gif} />);
+  return gifs.map((gif) => <img key={gif} src={gif} alt={gif} />);
 }
 
 export default GifList;
